3be845b229a050deb4d905260c0794c60ff069d1
Immler Ricardo <immler.ricardo@gmail.com>
2026-06-06 14:25:24 +0300

    Initial Commit

---
+++ Logbook/LogbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogbookApp.swift
+//  Logbook
+//
+//  Created by Immler Ricardo Noel on 06/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct LogbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
